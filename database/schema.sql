8ea9c243d547ba6f4aeee0c2327f810fbedece95
DevloperAmanSingh <amansinghneh211205@gmail.com>
2024-09-01 20:54:54 +0530

    feat: Update user table schema and queries
    The user table schema and queries have been updated to include additional fields and match the changes in the database schema. This update allows for better data management and retrieval.

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,65 @@
-CREATE TABLE users (
-	id uuid PRIMARY KEY,
-	email TEXT UNIQUE NOT NULL,
-	first_name TEXT NOT NULL,
-	last_name TEXT NOT NULL
+CREATE TABLE "user" (
+    "id" UUID NOT NULL UNIQUE,
+    "submissions" UUID,
+    "email" VARCHAR NOT NULL,
+    "regNo" VARCHAR NOT NULL,
+    "password" VARCHAR NOT NULL,
+    "role" CHAR NOT NULL,
+    "roundQualified" INTEGER NOT NULL,
+    "score" INTEGER,
+    "name" VARCHAR NOT NULL,
+    PRIMARY KEY("id")
 );
+
+
+
+CREATE TABLE "questions" (
+	"id" UUID NOT NULL UNIQUE,
+	"title" VARCHAR,
+	"description" TEXT,
+	"inputFormat" TEXT,
+	"outputFormat" TEXT,
+	"points" INTEGER,
+	"round" SMALLINT NOT NULL,
+	"constraints" TEXT,
+	"testcases" UUID,
+	PRIMARY KEY("id")
+);
+
+
+CREATE TABLE "submissions" (
+	"id" UUID NOT NULL UNIQUE,
+	"questionId" UUID NOT NULL,
+	"testcases_passed" INTEGER NOT NULL,
+	"testcases_failed" INTEGER NOT NULL,
+	"runtime" TIME NOT NULL,
+	"memory" VARCHAR NOT NULL,
+	"sub time" TIMESTAMP NOT NULL,
+	"testcases_id" UUID,
+	PRIMARY KEY("id")
+);
+
+
+CREATE TABLE "testcases" (
+	"id" UUID NOT NULL UNIQUE,
+	"expected_output" TEXT,
+	"memory" VARCHAR,
+	"input" TEXT,
+	"hidden" BOOLEAN,
+	"runtime" TIME,
+	PRIMARY KEY("id")
+);
+
+
+ALTER TABLE "user"
+ADD FOREIGN KEY("submissions") REFERENCES "submissions"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE "submissions"
+ADD FOREIGN KEY("questionId") REFERENCES "questions"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE "questions"
+ADD FOREIGN KEY("testcases") REFERENCES "testcases"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE "submissions"
+ADD FOREIGN KEY("testcases_id") REFERENCES "testcases"("id")
+ON UPDATE NO ACTION ON DELETE NO ACTION;
