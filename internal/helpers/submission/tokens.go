139ce4842f464470ee9e4d2228e89da5f956e738
Vaibhav Sijaria <139199971+vaibhavsijaria@users.noreply.github.com>
2024-09-07 18:44:30 +0530

    refactor: Refactor TokenManager functions to use submission ID instead of user ID

--- internal/helpers/submission/tokens.go
+++ internal/helpers/submission/tokens.go
@@ -15,7 +15,7 @@ type TokenManager struct {
 
 var Tokens *TokenManager
 
-func Init() error {
+func Init() {
 	host := os.Getenv("REDIS_HOST")
 	port := os.Getenv("REDIS_PORT")
 	pwd := os.Getenv("REDIS_PASSWORD")
@@ -32,38 +32,36 @@ func Init() error {
 	})
 
 	if err := client.Ping(context.Background()).Err(); err != nil {
-		fmt.Println("Redis Init Failed: " + err.Error())
-		return err
+		panic("Redis Init Failed: " + err.Error())
 	}
 	Tokens = &TokenManager{client: client}
-	return nil
 }
 
-func (tm *TokenManager) AddToken(ctx context.Context, token string, userID string) error {
-	err := tm.client.Set(ctx, fmt.Sprintf("token:%s", token), userID, 0).Err()
+func (tm *TokenManager) AddToken(ctx context.Context, token string, subID string) error {
+	err := tm.client.Set(ctx, fmt.Sprintf("token:%s", token), subID, 0).Err()
 	if err != nil {
 		return err
 	}
 
-	err = tm.client.SAdd(ctx, fmt.Sprintf("user:%s:tokens", userID), token).Err()
+	err = tm.client.SAdd(ctx, fmt.Sprintf("sub:%s:tokens", subID), token).Err()
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (tm *TokenManager) GetUserID(ctx context.Context, token string) (string, error) {
-	userID, err := tm.client.Get(ctx, fmt.Sprintf("token:%s", token)).Result()
+func (tm *TokenManager) GetSubID(ctx context.Context, token string) (string, error) {
+	subID, err := tm.client.Get(ctx, fmt.Sprintf("token:%s", token)).Result()
 	if err == redis.Nil {
 		return "", fmt.Errorf("token not found")
 	} else if err != nil {
 		return "", err
 	}
-	return userID, nil
+	return subID, nil
 }
 
 func (tm *TokenManager) DeleteToken(ctx context.Context, token string) error {
-	userID, err := tm.GetUserID(ctx, token)
+	subID, err := tm.GetSubID(ctx, token)
 	if err != nil {
 		return err
 	}
@@ -73,9 +71,21 @@ func (tm *TokenManager) DeleteToken(ctx context.Context, token string) error {
 		return err
 	}
 
-	err = tm.client.SRem(ctx, fmt.Sprintf("user:%s:tokens", userID), token).Err()
+	err = tm.client.SRem(ctx, fmt.Sprintf("sub:%s:tokens", subID), token).Err()
 	if err != nil {
 		return err
+	}
+
+	setSize, err := tm.client.SCard(ctx, fmt.Sprintf("sub:%s:tokens", subID)).Result()
+	if err != nil {
+		return err
+	}
+
+	if setSize == 0 {
+		err = tm.client.Del(ctx, fmt.Sprintf("sub:%s:tokens", subID)).Err()
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
