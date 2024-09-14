e13feb821a3bba81a02b693ffb79fe66a158cdc5
DevloperAmanSingh <amansinghneh211205@gmail.com>
2024-09-14 23:25:52 +0530

    refactor: Initialize task queue for asynchronous processing
    This commit refactors the code to initialize a task queue for asynchronous processing of submissions. It introduces a new package `queue` with a `queue.go` file that contains the necessary functions for initializing the queue and starting the queue server. The `main.go` file is modified to initialize the task server and client, and to start the queue server in a separate goroutine. This change improves the scalability and performance of the application by offloading time-consuming tasks to the task queue.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,20 +6,31 @@ import (
 	"github.com/CodeChefVIT/cookoff-backend/internal/helpers/auth"
 	database "github.com/CodeChefVIT/cookoff-backend/internal/helpers/database"
 	logger "github.com/CodeChefVIT/cookoff-backend/internal/helpers/logging"
+	"github.com/CodeChefVIT/cookoff-backend/internal/helpers/queue"
 	"github.com/CodeChefVIT/cookoff-backend/internal/helpers/submission"
 	"github.com/CodeChefVIT/cookoff-backend/internal/server"
+	"github.com/CodeChefVIT/cookoff-backend/internal/worker"
+	"github.com/hibiken/asynq"
 )
 
 func main() {
+	// Initialize services
 	logger.InitLogger()
 	database.Init()
 	database.InitCache()
 	auth.InitJWT()
 	submission.Init(database.RedisClient)
-	server := server.NewServer()
 
+	taskServer, taskClient := queue.InitQueue("redis:6379", 2)
+
+	go func() {
+		mux := asynq.NewServeMux()
+		mux.HandleFunc("submission:process", worker.ProcessSubmissionTask)
+		queue.StartQueueServer(taskServer, mux)
+	}()
+
+	server := server.NewServer(taskClient)
 	logger.Infof("Server started at port 8080")
-
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
