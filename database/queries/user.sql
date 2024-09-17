cfc7f9678cb0e93df26cdc345734585395bd09b2
Xenomorph07 <vedant.matanhelia@gmail.com>
2024-09-18 02:41:54 +0530

    fix: snake_case_schema

--- database/queries/user.sql
+++ database/queries/user.sql
@@ -1,16 +1,16 @@
 -- :name CreateUser :one
-INSERT INTO "user" (id, submissions, email, "regNo", password, role, "roundQualified", "score", name)
+INSERT INTO users (id, submissions, email, reg_no, password, role, round_qualified, score, name)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
 RETURNING *;
 -- name: GetUserByEmail :one
-SELECT id, email, "regNo", password, role, "roundQualified", "score", name
-FROM "user"
+SELECT id, email, reg_no, password, role, round_qualified, score, name
+FROM users
 WHERE email = $1;
 -- name: GetUserByUsername :one
-SELECT id, email, "regNo", password, role, "roundQualified", "score", name
-FROM "user"
+SELECT id, email, reg_no, password, role, round_qualified, score, name
+FROM users
 WHERE name = $1;
 -- name: GetUserById :one
-SELECT id, email, "regNo", password, role, "roundQualified", "score", name
-FROM "user"
+SELECT id, email, reg_no, password, role, round_qualified, score, name
+FROM users
 WHERE id = $1;
--- database/schema.sql
+++ database/schema.sql
@@ -1,66 +1,67 @@
-CREATE TABLE "user" (
-	"id" UUID NOT NULL UNIQUE,
-	"email" TEXT NOT NULL UNIQUE, 
-	"regNo" TEXT NOT NULL UNIQUE,
-	"password" TEXT NOT NULL,
-	"role" TEXT NOT NULL,
-	"roundQualified" INTEGER NOT NULL DEFAULT 0,
-	"score" INTEGER DEFAULT 0,
-	"name" TEXT NOT NULL,
-	PRIMARY KEY("id")
+CREATE TABLE users (
+	id UUID NOT NULL UNIQUE,
+	email TEXT NOT NULL UNIQUE, 
+	reg_no TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL,
+	role TEXT NOT NULL,
+	round_qualified INTEGER NOT NULL DEFAULT 0,
+	score INTEGER DEFAULT 0,
+	name TEXT NOT NULL,
+	PRIMARY KEY(id)
 );
 
-CREATE TABLE "questions" (
-	"id" UUID NOT NULL UNIQUE,
-	"description" TEXT,
-	"title" TEXT,
-	"inputFormat" TEXT,
-	"points" INTEGER,
-	"round" INTEGER NOT NULL,
-	"constraints" TEXT,
-	"outputFormat" TEXT,
-	PRIMARY KEY("id")
+CREATE TABLE questions (
+	id UUID NOT NULL UNIQUE,
+	description TEXT,
+	title TEXT,
+	input_format TEXT,
+	points INTEGER,
+	round INTEGER NOT NULL,
+	constraints TEXT,
+	output_format TEXT,
+	PRIMARY KEY(id)
 );
 
-CREATE TABLE "submissions" (
-	"id" UUID NOT NULL UNIQUE,
-	"question_id" UUID NOT NULL,
-	"testcases_passed" INTEGER DEFAULT 0,
-	"testcases_failed" INTEGER DEFAULT 0,
-	"runtime" DECIMAL,
-	"sub_time" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	"testcases_id" UUID,
-	"language_id" INTEGER NOT NULL,
-	"description" TEXT,
-	"memory" INTEGER,
-	"user_id" UUID,
-	"status" TEXT,
-	PRIMARY KEY("id")
+CREATE TABLE submissions (
+	id UUID NOT NULL UNIQUE,
+	question_id UUID NOT NULL,
+	testcases_passed INTEGER DEFAULT 0,
+	testcases_failed INTEGER DEFAULT 0,
+	runtime DECIMAL,
+	submission_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	testcase_id UUID,
+	language_id INTEGER NOT NULL,
+	description TEXT,
+	memory INTEGER,
+	user_id UUID,
+	status TEXT,
+	PRIMARY KEY(id)
 );
 
-CREATE TABLE "testcases" (
-	"id" UUID NOT NULL UNIQUE,
-	"expected_output" TEXT,
-	"memory" TEXT,
-	"input" TEXT,
-	"hidden" BOOLEAN,
-	"runtime" TIME,
-	"question_id" UUID NOT NULL,
-	PRIMARY KEY("id")
+CREATE TABLE testcases (
+	id UUID NOT NULL UNIQUE,
+	expected_output TEXT,
+	memory TEXT,
+	input TEXT,
+	hidden BOOLEAN,
+	runtime TIME,
+	question_id UUID NOT NULL,
+	PRIMARY KEY(id)
 );
 
-ALTER TABLE "submissions"
-ADD FOREIGN KEY("question_id") REFERENCES "questions"("id")
+-- Foreign keys
+ALTER TABLE submissions
+ADD FOREIGN KEY(question_id) REFERENCES questions(id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-ALTER TABLE "testcases"
-ADD FOREIGN KEY("question_id") REFERENCES "questions"("id")
+ALTER TABLE testcases
+ADD FOREIGN KEY(question_id) REFERENCES questions(id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-ALTER TABLE "submissions"
-ADD FOREIGN KEY("testcases_id") REFERENCES "testcases"("id")
+ALTER TABLE submissions
+ADD FOREIGN KEY(testcase_id) REFERENCES testcases(id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-ALTER TABLE "submissions"
-ADD FOREIGN KEY("user_id") REFERENCES "user"("id")
+ALTER TABLE submissions
+ADD FOREIGN KEY(user_id) REFERENCES users(id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
